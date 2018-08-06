fd55bea0623092b712f4a3ca691403c54e32610b
iOS <ios@wirless.local>
2018-08-06 16:33:59 +0800

    Initial Commit

---
+++ WQInstrument/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WQInstrument
+//
+//  Created by iOS on 2018/8/6.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WQInstrument/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WQInstrument
+//
+//  Created by iOS on 2018/8/6.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
